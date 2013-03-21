e3844b7e1c3afb6363c7e481ddf5aaaaff1f6d31
evheny0 <evheny0@gmail.com>
2013-03-21 22:04:54 +0300

    writed addition of parkings, not stable

--- main.c
+++ main.c
@@ -1,38 +1,129 @@
-/*Even not compiled yet*/
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#define arraySize 30
 
-/*const int arraySize = 30;*/
 const int stringSize = 30;
 
-struct list_t {
+struct parking_t {
     int size;
     int carsNum;
-    int cars[arraySize];
-    struct list_t *next;
+    int *cars;
+    struct parking_t *next;
     /*list of cars*/
 };
 
-void addList(struct list_t *list, int lastList);
+void checkKeys(char **argv);
+int inputNumericalData();
 
-int main(int argc, char const *argv[])
+int addParcking(struct parking_t *parking, int lastParking);
+void moveCars(struct parking_t *parking, int lastParking);
+void showAllParkings(struct parking_t *parking, int lastParking);
+void freeAll(struct parking_t *parking, int lastParking);
+
+int main(int argc, char **argv)
 {
-    struct list_t *list;
-    int lastList = 0;
-    list = (struct list_t *)malloc (sizeof (struct list_t));
-    list->next = (struct list_t *)malloc (sizeof (struct list_t));
-    addList(list, lastList); 
+    checkKeys(argv);
+    struct parking_t *parking;
+    parking = (struct parking_t *)malloc (sizeof(struct parking_t));
+    int lastParking = -1;
+    char command[stringSize];
+    parking = NULL;
+    while (1) {
+        printf("Enter command:\n"); 
+        printf("    1 - Add parking\n");
+        printf("    2 - Move cars\n");
+        printf("    3 - Show all parkings\n");
+        printf("    4 - Exit\n");
+        fgets(command, stringSize, stdin);
+        switch (command[0]) {
+        case '1':
+            lastParking = addParcking(parking, lastParking);
+            break;
+        case '2':
+            moveCars(parking, lastParking);
+            break;
+        case '3':
+            showAllParkings(parking, lastParking);
+            break;
+        case '4':
+            freeAll(parking, lastParking);
+            return 0;
+        default:
+            printf("Wrong command. Try again\n");
+            break;
+        }
+    }
     return 0;
 }
 
-void addList(struct list_t *list, int lastList)
+int addParcking(struct parking_t *parking, int lastParking)
 {
-    struct list_t *buffer;
-    if (!list) {
-        list = (struct list_t *)malloc (sizeof (struct list_t));
+    int i;
+    struct parking_t *temp;
+    if (lastParking == -1) {
+        temp = parking;
+        printf("1\n");
     }
-    buffer = (struct list_t *)malloc (sizeof (struct list_t));
-    list->next = buffer;
+    else {
+        temp = (struct parking_t *)malloc (sizeof(struct parking_t));
+        for (i = 0; i < lastParking; i++) {
+            parking = parking->next;
+        }
+        parking->next = temp;
+        printf("2\n");
+    }\
+    lastParking++;
+    printf("Enter the maximum number of cars in the parking: \n");
+    temp->size = inputNumericalData() + 1;
+    printf("3\n");
+    temp->carsNum = 0;
+    printf("4\n");
+    temp->cars = (int *)malloc (temp->size * sizeof(int));
+    printf("5\n");
+    printf("Parking #%d created.\n", lastParking + 1);
+    return lastParking;
+}
+
+void moveCars(struct parking_t *parking, int lastParking)
+{
+    int i;
+}
+
+void showAllParkings(struct parking_t *parking, int lastParking)
+{
+    int i;
+}
+
+void freeAll(struct parking_t *parking, int lastParking)
+{
+    int i;
+}
+
+int inputNumericalData()
+{
+    char buffer[stringSize];
+    int num;
+    do {
+        fgets(buffer, stringSize, stdin);
+        num = atoi(buffer);
+        if (num <= 0) {
+            printf("Incorrect value. Try again: ");
+            continue;
+        }
+        break;
+    } while (1);
+    return num;
+}
+
+void checkKeys(char **argv)
+{
+    if (!argv[1]) {
+        return;
+    }
+    if (!strcmp(argv[1], "-h")) {
+        printf("================================================================================\n");
+        printf("                                     MANUAL:\n\n");
+        printf("================================================================================\n");
+        exit(0);
+    }
 }
